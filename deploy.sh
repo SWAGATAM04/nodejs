154078b6c03eb68ac7a0d8ae5248cc5622b329d7
SWAGATAM04 <skundu012@gmail.com>
2022-02-26 07:04:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NA
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-${BUILD_NUMBER}.json
 #Register the task definition in the repository
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-${BUILD_NUMBER}.json --region ${REGION}
-SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
+SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION}`
 #Get latest revision
 REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION} | jq .taskDefinition.revision`
 #Create or update service
